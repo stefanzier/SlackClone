cf92f5dc02fd00493b143aedac92f5c882d352c9
Stefan Zier <szier@icloud.com>
2016-06-11 20:56:26 -0700

    Save a sent message in MongoDB

---
+++ client/app.js
@@ -0,0 +1,20 @@
+import { Template } from 'meteor/templating';
+
+Template.messages.helpers({
+  messages: Messages.find({})
+});
+
+Template.footer.events({
+  'keypress input': function(e) {
+    var inputVal = $('.input-box_text').val();
+    if(!!inputVal) {
+      var charCode = (typeof e.which == "number") ? e.which : e.keycode;
+      if(charCode == 13) {
+        e.stopPropagation();
+        Messages.insert({text: $('.input-box_text').val()});
+        $('.input-box_text').val("");
+        return false;
+      }
+    }
+  }
+});
