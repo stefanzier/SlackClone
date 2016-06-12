8d227e82fc26acb2c50c407666d21c370147496d
Stefan Zier <szier@icloud.com>
2016-06-12 09:59:24 -0700

    Added Username Field to User Signup

--- client/app.js
+++ client/app.js
@@ -4,6 +4,10 @@ Template.messages.helpers({
   messages: Messages.find({})
 });
 
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
+
 Template.footer.events({
   'keypress input': function(e) {
     var inputVal = $('.input-box_text').val();
